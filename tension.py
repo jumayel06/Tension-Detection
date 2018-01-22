358b0035794a4ef01383d13d6692145304546a64
Jumayel Islam <jumayel06@gmail.com>
2018-01-21 19:02:37 -0500

    Refactor preprocessing steps

--- tension.py
+++ tension.py
@@ -4,9 +4,8 @@ Jumayel Islam (c) 2017
 
 import textract
 import re
-from wnaffect import WNAffect
 import nltk
-from senti_classifier import senti_classifier
+import glob
 
 
 class Tension:
@@ -15,55 +14,48 @@ class Tension:
         self.segments = []
         self.ques = []
         self.ans = []
+        self.interviewer = ""
+        self.interviewee = ""
         self.corpus = textract.process(corpus).decode('utf_8').strip().replace(u'\xa0', u' ')
 
-
     def extract_segments(self):
+        initial_time = re.search(r'\d{2}:\d{2}:\d{2}', self.corpus).group()
+        initial_time_index = self.corpus.find(initial_time)
+        self.corpus = self.corpus[initial_time_index + 8:]
+        self.corpus = self.corpus.replace('\n', ' ')
+        words = self.corpus.split(' ')
+        candidates = []
+        for word in words:
+            if ":" in word:
+                candidates.append(word)
+        fdist = nltk.FreqDist(candidates)
+        candidate1 = fdist.most_common(2)[0][0].replace(':','')
+        candidate2 = fdist.most_common(2)[1][0].replace(':','')
+
+        if self.corpus.find(candidate1) < self.corpus.find(candidate2):
+            self.interviewer = candidate1
+            self.interviewee = candidate2
+        else:
+            self.interviewer = candidate2
+            self.interviewee = candidate1
 
         while True:
-            try:
-                time1_str = re.search(r'\d{2}:\d{2}:\d{2}', self.corpus).group()
-                time1_index = self.corpus.find(time1_str)
-                time2_str = re.search(r'\d{2}:\d{2}:\d{2}', self.corpus[time1_index + 8:]).group()
-                time2_index = self.corpus.find(time2_str)
-
-                self.segments.append(self.corpus[time1_index + 8:time2_index])
-                self.corpus = self.corpus[time2_index:]
-            except:
+            indices = [s.start() for s in re.finditer(self.interviewer+":", self.corpus)]
+            if len(indices) < 2:
+                self.segments.append(self.corpus[indices[0]:])
                 break
+            self.segments.append(self.corpus[indices[0]:indices[1]])
+            self.corpus = self.corpus[indices[1]:]
 
-        for seg in self.segments:
-            self.extract_ques_ans(seg)
-
-
-    def extract_ques_ans(self, str):
-
-        end = str.find('Tags')
-        tmp_str = str[:end]
-        start = tmp_str.rfind('?')
-
-        self.ques.append(str[0:start + 1].strip().replace('\n', ' '))
-        self.ans.append(tmp_str[start + 1:end].strip().replace('\n', ' '))
-
-
-    def extract_polarity_scores(self):
-
-        for question, answer in zip(self.ques, self.ans):
-            print(question)
-            print(answer)
-            pos_score, neg_score = senti_classifier.polarity_scores(nltk.sent_tokenize(answer))
-            print(pos_score, neg_score)
-            for ls in nltk.sent_tokenize(answer):
-                for word in nltk.word_tokenize(ls):
-                    emo = wna.get_emotion(word, nltk.pos_tag([word])[0][1])
-                    if emo is not None:
-                        print(emo)
-
+    def extract_ques_ans(self):
+        for segment in self.segments:
+            self.ques.append(segment[segment.find(self.interviewer)+len(self.interviewer)+1:
+                             segment.find(self.interviewee)].strip())
+            self.ans.append(segment[segment.find(self.interviewee) + len(self.interviewee) + 1:].strip())
 
 if __name__ == "__main__":
-    wordnet16 = '/home/jumayel/Downloads/wordnet-1.6'
-    wndomains32 = '/home/jumayel/Downloads/wn-domains-3.2'
-    wna = WNAffect(wordnet16, wndomains32)
-    tension = Tension('interview_scripts/interview_sample.doc')
-    tension.extract_segments()
-    tension.extract_polarity_scores()
+    corpora = glob.glob("interview_transcripts/*.docx")
+    for corpus in corpora:
+        tension = Tension(corpus)
+        tension.extract_segments()
+        tension.extract_ques_ans()
